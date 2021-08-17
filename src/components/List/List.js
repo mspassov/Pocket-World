4eab5455437dc4939d08c3e22c52ac57e368e352
Martin Spassov <mspassov@uwo.ca>
2021-08-17 10:54:23 -0400

    progress on Header, Map, List, and Card Details

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import PlaceDetails from '../PlaceDetails/PlaceDetails'
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import useStyles from "./styles";
 
 const List = () => {
-  return <h1>This is a list</h1>;
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+
+  const places = [{name: "place1"}, {name: "place2"}, {name: "place3"}];
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h6">
+        Restaurants, Hotels & Attractions Near You
+      </Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurant">Restaurant</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={type} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3 stars</MenuItem>
+          <MenuItem value={4}>Above 4 stars</MenuItem>
+          <MenuItem value={4.5}>Above 4.5 stars</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) =>{
+          return(
+            <Grid item key={i} xs={12}>
+              <PlaceDetails place={place}/>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
+  );
 };
 
 export default List;
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,29 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
 
+import useStyles from "./styles";
 const Map = () => {
-  return <h1>This is the map</h1>;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px");
+  const coordinates = { lat: 47.1164, lng: 101 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCiURCaJYMrk1FWHff3fFgoZAOc6BVmY6A" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
